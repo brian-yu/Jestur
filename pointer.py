cdfca8d07e09f11f1da4388690462eab8b64074e
Brian Yu <brian@byu.io>
2016-02-06 21:15:01 -0500

    hello

---
+++ pointer.py
@@ -0,0 +1,34 @@
+import sys
+from Quartz.CoreGraphics import CGEventCreateMouseEvent
+from Quartz.CoreGraphics import CGEventPost
+from Quartz.CoreGraphics import kCGEventMouseMoved
+from Quartz.CoreGraphics import kCGEventLeftMouseDown
+from Quartz.CoreGraphics import kCGEventLeftMouseDown
+from Quartz.CoreGraphics import kCGEventLeftMouseUp
+from Quartz.CoreGraphics import kCGMouseButtonLeft
+from Quartz.CoreGraphics import kCGHIDEventTap
+posx = float(sys.argv[1])
+posy = float(sys.argv[2])
+
+def mouseEvent(type, posx, posy):
+        theEvent = CGEventCreateMouseEvent(
+                    None, 
+                    type, 
+                    (posx,posy), 
+                    kCGMouseButtonLeft)
+        CGEventPost(kCGHIDEventTap, theEvent)
+
+def mousemove(posx,posy):
+        mouseEvent(kCGEventMouseMoved, posx,posy);
+
+def mouseclick(posx,posy):
+        # uncomment this line if you want to force the mouse 
+        # to MOVE to the click location first (I found it was not necessary).
+        mouseEvent(kCGEventMouseMoved, posx,posy);
+        mouseEvent(kCGEventLeftMouseDown, posx,posy);
+        mouseEvent(kCGEventLeftMouseUp, posx,posy);
+mousemove(posx, posy)
+# while True:
+#     for x in range(0, 1500, 100):
+#         for y in range(0, 900, 5):
+#             mousemove(x, y)
